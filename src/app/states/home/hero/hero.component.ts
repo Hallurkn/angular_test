d11dbcc820ccdc8171ed162a48089e4f2234c20d
Hallur Kárason Nielsen <hallurkn@hotmail.com>
2018-05-26 00:09:05 +0200

    wip, bedtime

--- src/app/states/home/hero/hero.component.ts
+++ src/app/states/home/hero/hero.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewChecked } from '@angular/core';
 
 @Component({
 	selector: 'hero-component',
 	templateUrl: './hero.component.html',
 	styleUrls: ['hero.component.scss'],
 })
-export class HeroComponent {
-	constructor() {
+export class HeroComponent implements AfterViewChecked {
+
+	public animateClass: string;
+
+	constructor() { }
+
+	ngAfterViewChecked() {
+		console.log('now');
 	}
 }
